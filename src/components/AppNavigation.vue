5b7171f32280ae57f0a167feb22d1fd871f35c0b
daiki-nishimura <72265603+daiki-nishimura@users.noreply.github.com>
2021-02-03 09:44:16 +0900

    appnavigationの作成 (#6)

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app-bar app color="#3DA9FC">
+    <!-- クラスの名前をいい感じにしてください -->
+    <div class="title">
+      <span class="titleName">JBC-check</span>
+    </div>
+    <div class="searchBar">
+      <!-- v-modelの値はdataと同じ値にする -->
+      <v-text-field
+        v-model="residentSearch"
+        class="search"
+        solo
+        dense
+        placeholder="利用者の検索"
+        append-icon="mdi-menu-down"
+        hide-details="auto"
+      ></v-text-field>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'AppNavigation',
+  data() {
+    return {
+      // この名前をいい感じにする
+      residentSearch: '',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.searchBar {
+  padding-left: 47px;
+}
+.search {
+  width: 640px;
+}
+.title {
+  padding-left: 45px;
+  padding-right: 47x;
+
+  .titleName {
+    font-size: 24px;
+    color: #ffff;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+}
+</style>
