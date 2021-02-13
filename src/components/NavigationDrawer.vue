8d39d29e7a7c23a4422f7c49ae43cdf69b3656fa
daiki-nishimura <daikiahe0@gmail.com>
2021-02-13 09:20:04 +0900

    indexの追加

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -7,7 +7,7 @@
     clipped
   >
     <v-list>
-      <v-list-item v-for="item in items" :key="item.title" link>
+      <v-list-item v-for="(item, index) in items" :key="item.title" link>
         <v-list-item-icon>
           <v-icon color="white">{{ item.icon }}</v-icon>
         </v-list-item-icon>
@@ -16,7 +16,7 @@
           <v-list-item-title class="white--text" v-text="item.title" />
         </v-list-item-content>
 
-        <v-btn v-if="showChevronIcon(item)" icon @click.stop="closeListTitle">
+        <v-btn v-if="!index" icon @click.stop="closeListTitle">
           <v-icon color="white">mdi-chevron-left</v-icon>
         </v-btn>
       </v-list-item>
