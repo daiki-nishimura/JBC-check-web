85e747bad69ad5efbd0d99a59cc6a7902ed5fb07
daiki-nishimura <daikiahe0@gmail.com>
2021-02-24 23:01:16 +0900

    refactor: コードの見直し

---
+++ src/components/ResidentContentMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card tile elevation="0">
+    <v-toolbar flat color="blue-grey lighten-4">
+      <v-btn icon>
+        <v-icon x-large>mdi-plus-circle</v-icon>
+      </v-btn>
+
+      <v-spacer />
+
+      <ContentHeaderDate />
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+import ContentHeaderDate from '@/components/common/ContentHeaderDate.vue';
+
+export default {
+  name: 'ResidentContentMenu',
+
+  components: {
+    ContentHeaderDate,
+  },
+};
+</script>
