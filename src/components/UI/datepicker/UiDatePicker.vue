cac9230d45853fdcc151045c5097df9569ad59c3
daiki-nishimura <daikiahe0@gmail.com>
2021-02-24 23:03:13 +0900

    feat: コンポーネント化しカレンダーを追加

---
+++ src/components/UI/datepicker/UiDatePicker.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-menu offset-y>
+    <template #activator="{ on }">
+      <v-btn icon v-on="on">
+        <v-icon x-large>mdi-calendar-blank</v-icon>
+      </v-btn>
+    </template>
+    <v-date-picker
+      v-model="picker"
+      :day-format="(date) => new Date(date).getDate()"
+      no-title
+      locale="ja"
+    />
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: 'UiDatePicker',
+
+  data() {
+    return {
+      date: undefined,
+    };
+  },
+
+  computed: {
+    picker: {
+      get() {
+        return this.date;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+  },
+};
+</script>
