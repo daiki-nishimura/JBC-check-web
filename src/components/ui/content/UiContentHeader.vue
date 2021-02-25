ea99f5d0b8af56bc94a12478994ea54ec9b09907
daiki-nishimura <daikiahe0@gmail.com>
2021-02-26 00:15:50 +0900

    feat: UiContentHeaderを追加

---
+++ src/components/ui/content/UiContentHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card tile elevation="0">
+    <v-toolbar flat color="blue-grey lighten-4">
+      <slot />
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'UiContentHeader',
+  data() {
+    return {};
+  },
+};
+</script>
