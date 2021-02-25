3a396722a19e66797c2f168239adef2c67141ae4
daiki-nishimura <daikiahe0@gmail.com>
2021-02-25 16:03:43 +0900

    style:jsdocの追加

--- src/utils/date.js
+++ src/utils/date.js
@@ -7,12 +7,15 @@ dayjs.locale('ja');
  * 日付を指定した形式に変換する
  * @param {string} value 日付
  * @param {string} format 形式
- * @return {string} formatの形式で戻る
+ * @return {string} formatの形式で返す
  */
 export function convertDateFormat(value, format) {
   return dayjs(value).format(format);
 }
-
+/**
+ * 日付の形式をYYY/MM/DDに変換する
+ * @return {string} YYY/MM/DDの形式で返す
+ */
 export function getCurrentDate(format = 'YYYY/MM/DD') {
   return dayjs().format(format);
 }
