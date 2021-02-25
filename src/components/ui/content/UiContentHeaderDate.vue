aef8bbeb575b7983e4cc7c1393f4ee1f8b4fede1
daiki-nishimura <daikiahe0@gmail.com>
2021-02-26 00:13:33 +0900

    refactor: 名前の変更

---
+++ src/components/ui/content/UiContentHeaderDate.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <UiDatePicker @input="setDisplayDate" />
+    <span class="text-h5">{{ displayDate }}</span>
+  </div>
+</template>
+
+<script>
+import { convertDateFormat, getCurrentDate } from '@/utils/date';
+import UiDatePicker from '@/components/ui/date-picker/UiDatePicker.vue';
+
+export default {
+  name: 'UiContentHeaderDate',
+
+  components: {
+    UiDatePicker,
+  },
+
+  data() {
+    return {
+      /**
+       * 表示する日付
+       */
+      displayDate: getCurrentDate('YYYY/MM/DD (ddd)'),
+    };
+  },
+
+  methods: {
+    /**
+     * 表示する日付をセットする
+     */
+    setDisplayDate(date) {
+      this.displayDate = convertDateFormat(date, 'YYYY/MM/DD (ddd)');
+    },
+  },
+};
+</script>
