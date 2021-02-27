ee4a2840d2ee1e1ba37ea408da794221af70c94c
daiki-nishimura <daikiahe0@gmail.com>
2021-02-27 19:58:13 +0900

    feat/<ResidentDataTableの追加

--- src/views/ResidentList.vue
+++ src/views/ResidentList.vue
@@ -1,15 +1,21 @@
 <template>
-  <ResidentContentHeader />
+  <div>
+    <ResidentContentHeader />
+
+    <ResidentDataTable />
+  </div>
 </template>
 
 <script>
 import ResidentContentHeader from '@/components/resident/ResidentContentHeader.vue';
+import ResidentDataTable from '@/components/resident/ResidentDataTable.vue';
 
 export default {
   name: 'ResidentList',
 
   components: {
     ResidentContentHeader,
+    ResidentDataTable,
   },
 };
 </script>
