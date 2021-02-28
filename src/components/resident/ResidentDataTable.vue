52cbcc033219086b9e7f754e32e457db67bd3de7
daiki-nishimura <daikiahe0@gmail.com>
2021-02-28 15:58:52 +0900

    feat: レイアウトの調整

--- src/components/resident/ResidentDataTable.vue
+++ src/components/resident/ResidentDataTable.vue
@@ -1,16 +1,19 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="利用者名"
-    item-key="name"
-    show-select
-    class="elevation-1 data-table"
-    height="720"
-  >
-    <template v-slot:items="props">
-      <td>{{ props.item.ApplicationType }}</td>
-    </template>
-  </v-data-table>
+  <v-container>
+    <v-data-table
+      :headers="headers"
+      :items="利用者名"
+      item-key="name"
+      show-select
+      class="elevation-1 data-table"
+      style="width: 100%"
+      height="768"
+    >
+      <!-- <template v-slot:items="props">
+          <td>{{ props.item.ApplicationType }}</td>
+        </template> -->
+    </v-data-table>
+  </v-container>
 </template>
 
 <script>
@@ -18,102 +21,145 @@ export default {
   name: 'ResidentDataTable',
   data() {
     return {
-      singleSelect: false,
       selected: [],
       headers: [
         {
-          text: '利用者',
+          text: '利用者名',
           align: 'start',
           sortable: false,
-          value: 'value',
-          width: String,
+          value: 'name',
         },
-        { text: '生年月日', value: '生年月日', width: '20%' },
+
+        { text: '生年月日', value: '生年月日', width: '' },
         { text: '性別', value: '性別' },
         { text: '身長', value: '身長' },
         { text: '体重', value: '体重' },
         { text: 'アレルギー', value: 'アレルギー' },
+        { text: '病歴', value: '病歴' },
+        { text: '趣味趣向', value: '趣味趣向' },
+        { text: '生活歴', value: '生活歴' },
+        { text: '備考', value: '備考' },
       ],
       利用者名: [
         {
-          name: 'Frozen Yogurt',
-          生年月日: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%',
+          name: '西村だいき',
+          生年月日: '',
+          性別: '',
+          身長: 'cm',
+          体重: 'kg',
+          アレルギー: '',
+          病歴: '',
+          趣味趣向: '',
+          生活歴: '',
+          備考: 'こんにちは',
         },
         {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%',
+          name: '',
+          生年月日: '',
+          性別: '',
+          身長: 'cm',
+          体重: 'kg',
+          アレルギー: '',
+          病歴: '',
+          趣味趣向: '',
+          生活歴: '',
+          備考: '',
         },
         {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%',
+          name: ' ',
+          生年月日: '',
+          性別: '',
+          身長: 'cm',
+          体重: 'kg',
+          アレルギー: '',
+          病歴: '',
+          趣味趣向: '',
+          生活歴: '',
+          備考: '',
         },
         {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%',
+          name: ' ',
+          生年月日: '',
+          性別: '',
+          身長: 'cm',
+          体重: 'kg',
+          アレルギー: '',
+          病歴: '',
+          趣味趣向: '',
+          生活歴: '',
+          備考: '',
         },
         {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%',
+          name: ' ',
+          生年月日: '',
+          性別: '',
+          身長: 'cm',
+          体重: 'kg',
+          アレルギー: '',
+          病歴: '',
+          趣味趣向: '',
+          生活歴: '',
+          備考: '',
         },
         {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%',
+          name: ' ',
+          生年月日: '',
+          性別: '',
+          身長: 'cm',
+          体重: 'kg',
+          アレルギー: '',
+          病歴: '',
+          趣味趣向: '',
+          生活歴: '',
+          備考: '',
         },
         {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%',
+          name: ' ',
+          生年月日: '',
+          性別: '',
+          身長: 'cm',
+          体重: 'kg',
+          アレルギー: '',
+          病歴: '',
+          趣味趣向: '',
+          生活歴: '',
+          備考: '',
         },
         {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%',
+          name: ' ',
+          生年月日: '',
+          性別: '',
+          身長: 'cm',
+          体重: 'kg',
+          アレルギー: '',
+          病歴: '',
+          趣味趣向: '',
+          生活歴: '',
+          備考: '',
         },
         {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%',
+          name: ' ',
+          生年月日: '',
+          性別: '',
+          身長: 'cm',
+          体重: 'kg',
+          アレルギー: '',
+          病歴: '',
+          趣味趣向: '',
+          生活歴: '',
+          備考: '',
         },
         {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%',
+          name: ' ',
+          生年月日: '',
+          性別: '',
+          身長: 'cm',
+          体重: 'kg',
+          アレルギー: '',
+          病歴: '',
+          趣味趣向: '',
+          生活歴: '',
+          備考: '',
         },
       ],
     };
@@ -123,6 +169,24 @@ export default {
 
 <style lang="scss">
 .data-table {
-  font-size: 40px !important;
+  &.v-data-table {
+    thead {
+      tr {
+        height: 88px;
+        th {
+          font-size: 18px !important;
+          background: #42a5f5;
+        }
+      }
+    }
+  }
+}
+tbody {
+  tr {
+    height: 68px;
+    td {
+      font-size: 18px !important;
+    }
+  }
 }
 </style>
