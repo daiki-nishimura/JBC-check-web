a55bb8030596552514479fb43a7176a92a35006a
daiki-nishimura <daikiahe0@gmail.com>
2021-03-01 15:51:27 +0900

    refactor: オブジェクト名の変更

---
+++ src/common/constants/contentDataTableHeadars.js
@@ -0,0 +1,14 @@
+export default {
+  RESIDENT: [
+    { text: '利用者名', value: 'name' },
+    { text: '生年月日', value: 'birthday' },
+    { text: '性別', value: 'gender' },
+    { text: '身長', value: 'height' },
+    { text: '体重', value: 'weight' },
+    { text: 'アレルギー', value: 'allergies' },
+    { text: '病歴', value: 'medicalHistory' },
+    { text: '趣味嗜好', value: 'preference' },
+    { text: '生活歴', value: 'lifeHistory' },
+    { text: '備考', value: 'remarks' },
+  ],
+};
