6cbd28d2da1d7e1b8f31bfd43833b9ad576b5a92
daiki-nishimura <daikiahe0@gmail.com>
2021-03-01 22:11:29 +0900

    refactor: コンポーネント化　現状

---
+++ src/components/ui/content/UiContentDataTable.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      item-key="id"
+      show-select
+      class="elevation-1 data-table"
+      height="768"
+      :footer-props="{
+        'items-per-page-text': '表示数',
+      }"
+    >
+      <slot />
+    </v-data-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'UiContentDataTable',
+};
+</script>
