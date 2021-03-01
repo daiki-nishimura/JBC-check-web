9d5282a8433d96fadbac696995cdb00d3719ef56
daiki-nishimura <daikiahe0@gmail.com>
2021-03-01 14:48:09 +0900

    refactor: preference スペースが入っていた為修正

--- src/common/constants/residentDataTableheadars.js
+++ src/common/constants/residentDataTableheadars.js
@@ -7,7 +7,7 @@ export default {
     { text: '体重', value: 'weight' },
     { text: 'アレルギー', value: 'allergies' },
     { text: '病歴', value: 'medicalHistory' },
-    { text: '趣味嗜好', value: 'preference ' },
+    { text: '趣味嗜好', value: 'preference' },
     { text: '生活歴', value: 'lifeHistory' },
     { text: '備考', value: 'remarks' },
   ],
