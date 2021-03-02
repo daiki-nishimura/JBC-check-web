491867e0d38ee1b41a3ecf28175aa286cd000ff5
daiki-nishimura <72265603+daiki-nishimura@users.noreply.github.com>
2021-03-02 17:36:36 +0900

    Feat/resident data tableの作成 (#27)

---
+++ src/components/common/ContentDataTable.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      item-key="id"
+      show-select
+      class="elevation-1 data-table"
+      height="768"
+      :footer-props="{
+        'items-per-page-text': '表示数',
+      }"
+    >
+      <slot />
+    </v-data-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ContentDataTable',
+  props: {
+    headers: {
+      type: Array,
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.data-table {
+  &.v-data-table {
+    thead {
+      tr {
+        height: 88px;
+        th {
+          font-size: 18px !important;
+          background: #42a5f5;
+          white-space: nowrap;
+        }
+      }
+    }
+    tbody {
+      tr {
+        height: 68px;
+        td {
+          font-size: 16px !important;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          max-width: 120px;
+        }
+      }
+    }
+  }
+}
+</style>
