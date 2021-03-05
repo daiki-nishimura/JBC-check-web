b71afd51c28bccdee596258ffd2f58e7edfb6cf0
daiki-nishimura <daikiahe0@gmail.com>
2021-03-05 18:22:48 +0900

    feat: ContentRegisterの作成

---
+++ src/components/common/ContentRegister.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <!-- 画面サイズを利用者一覧画面と同じようにしようとするとheightの値が大きくなります -->
+    <v-card height="880">
+      <v-card-text>
+        <slot />
+
+        <v-card-actions class="justify-center">
+          <div class="ma-3" style="position: absolute; bottom: 0">
+            <!-- キャンセルボタンを押した時ページ遷移で利用者一覧に戻るようにするべきなんでしょうか？ -->
+            <v-btn color="error" class="mr-4"> キャンセル </v-btn>
+
+            <v-btn class="white--text" color="blue lighten-1"> 送信 </v-btn>
+          </div>
+        </v-card-actions>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: `ContentRegister`,
+};
+</script>
