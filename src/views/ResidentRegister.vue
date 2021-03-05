db6ac2e7e1735b901a9dca7926994b0bc684c706
daiki-nishimura <daikiahe0@gmail.com>
2021-03-05 18:24:25 +0900

    feat: ResidentRegister.vueの作成

---
+++ src/views/ResidentRegister.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ResidentRegisterView />
+  </div>
+</template>
+
+<script>
+import ResidentRegisterView from '@/components/resident/ResidentRegisterView.vue';
+
+export default {
+  name: 'ResidentRegister',
+
+  components: {
+    ResidentRegisterView,
+  },
+};
+</script>
