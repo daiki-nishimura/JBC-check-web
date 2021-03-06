8141a49d675600055720f22682cb0439154f2205
daiki-nishimura <daikiahe0@gmail.com>
2021-03-06 17:57:01 +0900

    refactor: レイアウト調整

--- src/components/common/ContentRegister.vue
+++ src/components/common/ContentRegister.vue
@@ -1,19 +1,20 @@
 <template>
   <v-container>
-    <!-- 画面サイズを利用者一覧画面と同じようにしようとするとheightの値が大きくなります -->
-    <v-card height="880">
-      <v-card-text>
-        <slot />
+    <v-row justify="center">
+      <v-card class="mt-8" height="800" width="800">
+        <v-card-text>
+          <slot />
 
-        <v-card-actions class="justify-center">
-          <div class="ma-3" style="position: absolute; bottom: 0">
-            <v-btn color="error" class="mr-4"> </v-btn>
+          <v-card-actions class="justify-center">
+            <div class="ma-3" style="position: absolute; bottom: 0">
+              <v-btn color="error" class="mr-4"> </v-btn>
 
-            <v-btn class="white--text" color="blue lighten-1"> </v-btn>
-          </div>
-        </v-card-actions>
-      </v-card-text>
-    </v-card>
+              <v-btn class="white--text" color="blue lighten-1"> </v-btn>
+            </div>
+          </v-card-actions>
+        </v-card-text>
+      </v-card>
+    </v-row>
   </v-container>
 </template>
 
