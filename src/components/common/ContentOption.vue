43feee9d918c95b8d8627a2d0003c23e82f518cd
daiki-nishimura <daikiahe0@gmail.com>
2021-03-07 17:23:15 +0900

    refctor: v-btnの移動

--- src/components/common/ContentOption.vue
+++ src/components/common/ContentOption.vue
@@ -5,13 +5,7 @@
       <v-card-text>
         <slot />
 
-        <v-card-actions class="justify-center">
-          <div class="ma-3" style="position: absolute; bottom: 0">
-            <v-btn color="error" class="mr-4"> </v-btn>
-
-            <v-btn class="white--text" color="blue lighten-1"> </v-btn>
-          </div>
-        </v-card-actions>
+        <v-card-actions class="justify-center"> </v-card-actions>
       </v-card-text>
     </v-card>
   </v-container>
