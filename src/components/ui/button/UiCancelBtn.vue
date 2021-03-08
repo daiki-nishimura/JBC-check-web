0cdc9c3dac6d1aaa8187f306142145578e8e7f41
daiki-nishimura <daikiahe0@gmail.com>
2021-03-08 17:54:24 +0900

    refactor: canceleボタンのコンポーネント化

---
+++ src/components/ui/button/UiCancelBtn.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card-actions class="justify-center">
+    <div class="text-center">
+      <v-btn class="mr-4" color="blue-grey darken-2 white--text"><slot>キャンセル</slot></v-btn>
+    </div>
+  </v-card-actions>
+</template>
+
+<script>
+export default {
+  name: 'UiCancelBtn',
+};
+</script>
