f6f62f71006100c6118beaed3aaef1d50d1194ec
daiki-nishimura <daikiahe0@gmail.com>
2021-03-08 23:38:56 +0900

    refactor: btnファイルの修正

---
+++ src/components/resident/ResidentRegisterAction.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card-actions class="justify-center">
+    <div class="ma-3" style="position: absolute; bottom: 0">
+      <v-btn color="error" class="mr-4">キャンセル</v-btn>
+
+      <v-btn class="white--text" color="blue lighten-1">登録</v-btn>
+    </div>
+  </v-card-actions>
+</template>
+
+<script>
+export default {
+  name: 'ResidentRegisterAction',
+};
+</script>
