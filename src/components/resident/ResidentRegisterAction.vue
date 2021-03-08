1dd163ea0f6be31a99d31ff50cb729c3e6cf830f
daiki-nishimura <daikiahe0@gmail.com>
2021-03-09 01:16:39 +0900

    refactor: cancelボタンの色修正

--- src/components/resident/ResidentRegisterAction.vue
+++ src/components/resident/ResidentRegisterAction.vue
@@ -1,9 +1,9 @@
 <template>
   <v-card-actions class="justify-center">
     <div class="ma-3" style="position: absolute; bottom: 0">
-      <v-btn color="error" class="mr-4">キャンセル</v-btn>
+      <v-btn class="mr-4" color="cancel">キャンセル</v-btn>
 
-      <v-btn class="white--text" color="blue lighten-1">登録</v-btn>
+      <v-btn class="white--text" color="primary">登録</v-btn>
     </div>
   </v-card-actions>
 </template>
