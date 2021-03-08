869fa48d4a1723f74350d493d305542446ba90f7
daiki-nishimura <daikiahe0@gmail.com>
2021-03-08 17:55:06 +0900

    refactor: Registerボタンのコンポーネント化

---
+++ src/components/ui/button/UiRegisterBtn.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card-actions class="justify-center">
+    <div class="ma-3">
+      <v-btn class="white--text" color="primary"><slot>登録</slot></v-btn>
+    </div>
+  </v-card-actions>
+</template>
+
+<script>
+export default {
+  name: 'UiCancelBtn',
+};
+</script>
