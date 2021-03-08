cf53b0d309d0771227d4b45721628044e8e9e133
daiki-nishimura <daikiahe0@gmail.com>
2021-03-08 17:59:51 +0900

    refactor: 新規利用者登録画面レイアウト調整

--- src/components/common/ContentOption.vue
+++ src/components/common/ContentOption.vue
@@ -1,7 +1,6 @@
 <template>
   <v-container>
-    <!-- 画面サイズを利用者一覧画面と同じようにしようとするとheightの値が大きくなります -->
-    <v-card height="880">
+    <v-card class="mx-auto" height="840" max-width="800">
       <v-card-text>
         <slot />
       </v-card-text>
