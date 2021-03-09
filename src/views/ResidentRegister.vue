59376e21dad92dbb29ecb472f1347fd3af064958
daiki-nishimura <daikiahe0@gmail.com>
2021-03-09 15:07:47 +0900

    refactor: ResidentRegisterViewのファイル名変更に伴い修正

--- src/views/ResidentRegister.vue
+++ src/views/ResidentRegister.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <ResidentRegisterView />
+    <ResidentEditor />
   </div>
 </template>
 
 <script>
-import ResidentRegisterView from '@/components/resident/ResidentRegisterView.vue';
+import ResidentEditor from '@/components/resident/ResidentEditor.vue';
 
 export default {
   name: 'ResidentRegister',
 
   components: {
-    ResidentRegisterView,
+    ResidentEditor,
   },
 };
 </script>
