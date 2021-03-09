19443a9fc7a9ec2b7bb27d408ea24d3fc4cd5f4e
daiki-nishimura <daikiahe0@gmail.com>
2021-03-09 13:13:49 +0900

    refactor: styleの削除

--- src/components/resident/ResidentRegisterAction.vue
+++ src/components/resident/ResidentRegisterAction.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card-actions class="justify-center">
-    <div class="ma-3" style="position: absolute; bottom: 0">
+    <div class="ma-3">
       <v-btn class="mr-4" color="cancel">キャンセル</v-btn>
 
       <v-btn class="white--text" color="primary">登録</v-btn>
