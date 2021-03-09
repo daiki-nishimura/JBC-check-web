1e8457e133c68b4cd8d23a2825d0e7cf2eec3730
daiki-nishimura <daikiahe0@gmail.com>
2021-03-09 22:23:24 +0900

    refactor: 各v-modelを修正

--- src/components/resident/ResidentEditor.vue
+++ src/components/resident/ResidentEditor.vue
@@ -2,23 +2,23 @@
   <ContentOption>
     <v-text-field v-model="name" label="利用者名"></v-text-field>
 
-    <v-text-field v-model="name" label="生年月日"></v-text-field>
+    <v-text-field v-model="birthday" label="生年月日"></v-text-field>
 
     <v-select v-model="gender" :items="genderItems" label="性別"></v-select>
 
-    <v-text-field v-model="name" label="身長"></v-text-field>
+    <v-text-field v-model="height" label="身長"></v-text-field>
 
-    <v-text-field v-model="name" label="体重"></v-text-field>
+    <v-text-field v-model="weight" label="体重"></v-text-field>
 
-    <v-text-field v-model="name" label="アレルギー"></v-text-field>
+    <v-text-field v-model="allergies" label="アレルギー"></v-text-field>
 
-    <v-text-field v-model="name" label="病歴"></v-text-field>
+    <v-text-field v-model="medicalHistory" label="病歴"></v-text-field>
 
-    <v-text-field v-model="name" label="趣味嗜好"></v-text-field>
+    <v-text-field v-model="preference" label="趣味嗜好"></v-text-field>
 
-    <v-text-field v-model="name" label="生活歴"></v-text-field>
+    <v-text-field v-model="lifeHistory" label="生活歴"></v-text-field>
 
-    <v-text-field v-model="name" label="備考"></v-text-field>
+    <v-text-field v-model="remarks" label="備考"></v-text-field>
 
     <v-row class="mt-14" justify="center">
       <ResidentRegisterAction />
@@ -44,8 +44,46 @@ export default {
        * セレクトリストの選択肢
        */
       genderItems: ['男', '女'],
+      /**
+       * 利用者名
+       */
       name: '',
+      /**
+       * 生年月日
+       */
+      birthday: '',
+      /**
+       * 性別
+       */
       gender: '',
+      /**
+       * 身長
+       */
+      height: '',
+      /**
+       * 体重
+       */
+      weight: '',
+      /**
+       * アレルギー
+       */
+      allergies: '',
+      /**
+       * 病歴
+       */
+      medicalHistory: '',
+      /**
+       * 趣味嗜好
+       */
+      preference: '',
+      /**
+       * 生活歴
+       */
+      lifeHistory: '',
+      /**
+       * 備考
+       */
+      remarks: '',
     };
   },
   computed: {
@@ -55,7 +93,15 @@ export default {
       },
       set(value) {
         this.name = value;
+        this.birthday = value;
         this.gender = value;
+        this.height = value;
+        this.weight = value;
+        this.allergies = value;
+        this.medicalHistory = value;
+        this.preference = value;
+        this.lifeHistory = value;
+        this.remarks = value;
       },
     },
   },
