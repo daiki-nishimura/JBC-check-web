7abf0168fc10ea599b58bd3a1864e2f39afd2bd7
daiki-nishimura <daikiahe0@gmail.com>
2021-03-10 16:34:51 +0900

    refactor: btnのstyleをコンポーネント化

---
+++ src/components/common/ContentEditorButtons.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="content-editor-buttons">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.content-editor-buttons {
+  .v-btn {
+    width: 160px;
+  }
+  // +を記述した場合キャンセル側のmargin指定がききませんでしたので、.v-btn.v-btnという形にすると両方のボタンにmargin指定がききました
+  // レビュー後コメント消します
+  .v-btn.v-btn {
+    margin-right: 8px;
+    margin-left: 8px;
+  }
+}
+</style>
