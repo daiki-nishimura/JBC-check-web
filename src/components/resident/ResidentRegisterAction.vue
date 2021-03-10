fa0f86a7fd908034b0bf9a5072fd33882dc12f50
daiki-nishimura <daikiahe0@gmail.com>
2021-03-10 16:36:09 +0900

    refactor: コンポーネントContentEditorButtonsを追加

--- src/components/resident/ResidentRegisterAction.vue
+++ src/components/resident/ResidentRegisterAction.vue
@@ -1,15 +1,23 @@
 <template>
   <v-card-actions class="justify-center">
-    <div class="ma-3">
-      <v-btn class="mr-4" color="cancel">キャンセル</v-btn>
+    <ContentEditorButtons>
+      <div class="ma-3">
+        <v-btn class="mr-4" color="cancel">キャンセル</v-btn>
 
-      <v-btn class="white--text" color="primary">登録</v-btn>
-    </div>
+        <v-btn class="white--text" color="primary">登録</v-btn>
+      </div>
+    </ContentEditorButtons>
   </v-card-actions>
 </template>
 
 <script>
+import ContentEditorButtons from '@/components/common/ContentEditorButtons.vue';
+
 export default {
   name: 'ResidentRegisterAction',
+
+  components: {
+    ContentEditorButtons,
+  },
 };
 </script>
