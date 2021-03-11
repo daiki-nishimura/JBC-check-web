a87b05977b417182b16025ca9a4ad8de8a9a4a7f
daiki-nishimura <daikiahe0@gmail.com>
2021-03-11 11:51:40 +0900

    refactor: コメントの削除

--- src/components/common/ContentEditorButtons.vue
+++ src/components/common/ContentEditorButtons.vue
@@ -9,8 +9,6 @@
   .v-btn {
     width: 160px;
   }
-  // +を記述した場合キャンセル側のmargin指定がききませんでしたので、.v-btn.v-btnという形にすると両方のボタンにmargin指定がききました
-  // レビュー後コメント消します
   .v-btn.v-btn {
     margin-right: 8px;
     margin-left: 8px;
