159b19e0abc411dfcec5e78a73a2827308dbf238
daiki-nishimura <daikiahe0@gmail.com>
2021-03-11 13:53:38 +0900

    refactor: コンポーネントの追加

--- src/views/ResidentRegister.vue
+++ src/views/ResidentRegister.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <ResidentEditor />
+    <ContentEditor>
+      <ResidentEditor />
+      <v-row class="mt-14" justify="center">
+        <ResidentRegisterAction />
+      </v-row>
+    </ContentEditor>
   </div>
 </template>
 
 <script>
 import ResidentEditor from '@/components/resident/ResidentEditor.vue';
+import ContentEditor from '@/components/common/ContentEditor.vue';
+import ResidentRegisterAction from '@/components/resident/ResidentRegisterAction.vue';
 
 export default {
-  name: 'ResidentRegister',
+  name: `ResidentRegister`,
 
   components: {
+    ContentEditor,
     ResidentEditor,
+    ResidentRegisterAction,
   },
 };
 </script>
