ad47aaf2daceb960b4352982f58b03836bac087c
daiki-nishimura <daikiahe0@gmail.com>
2021-03-19 10:22:18 +0900

    refactor: mutationsの追記

--- src/store/resident/residentEditor.js
+++ src/store/resident/residentEditor.js
@@ -1,7 +1,5 @@
-// stateを作っていく
-// JSDocも忘れずに
 export const state = {
-    editingData: {
+  editingData: {
     name: '',
     birthday: '',
     gender: '',
@@ -11,29 +9,52 @@ export const state = {
     medicalHistory: '',
     preference: '',
     lifeHistory: '',
-    remarks: ''
+    remarks: '',
   },
 };
 
 export const mutations = {
-    name(state, { name }) {
-        state.editingData.name = name;
-    
-    birthday(state, { birthday }) 
-        state.editingData.birthday = birthday;
-    }
+  name(state, { name }) {
+    state.editingData.name = name;
   },
+  birthday(state, { birthday }) {
+    state.editingData.birthday = birthday;
+  },
+  gender(state, { gender }) {
+    state.editingData.gender = gender;
+  },
+  height(state, { height }) {
+    state.editingData.height = height;
+  },
+  weight(state, { weight }) {
+    state.editingData.weight = weight;
+  },
+  allergies(state, { allergies }) {
+    state.editingData.allergies = allergies;
+  },
+  medicalHistory(state, { medicalHistory }) {
+    state.editingData.medicalHistory = medicalHistory;
+  },
+  preference(state, { preference }) {
+    state.editingData.preference = preference;
+  },
+  lifeHistory(state, { lifeHistory }) {
+    state.editingData.lifeHistory = lifeHistory;
+  },
+  remarks(state, { remarks }) {
+    state.editingData.remarks = remarks;
+  },
+};
 
 export const actions = {
   setName({ commit }, { name }) {
     commit('name', { name });
-  }
-}
+  },
+};
 
 export default {
   namespaced: true,
   state,
   mutations,
   actions,
-}
-
+};
