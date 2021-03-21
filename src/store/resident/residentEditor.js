3d5ae07d696d781ce1132ad4f4eea625aeb54cc9
daiki-nishimura <daikiahe0@gmail.com>
2021-03-21 09:23:21 +0900

    refactor: residentEditor.jsのJSdoc 修正

--- src/store/resident/residentEditor.js
+++ src/store/resident/residentEditor.js
@@ -15,81 +15,81 @@ export const state = {
 
 export const mutations = {
   /**
-   * 利用者名の更新
-   * @param {state} state
-   * @param {function} name
+   * 利用者名の変更
+   * @param  state
+   * @param  name
    */
   name(state, { name }) {
     state.editingData.name = name;
   },
   /**
-   * 生年月日の更新
-   * @param {state} state
-   * @param {function} birthday
+   * 生年月日の変更
+   * @param  state
+   * @param  birthday
    */
   birthday(state, { birthday }) {
     state.editingData.birthday = birthday;
   },
   /**
-   * 性別の更新
-   * @param {state} state
-   * @param {function} gender
+   * 性別の
+   * @param state
+   * @param gender
    */
   gender(state, { gender }) {
     state.editingData.gender = gender;
   },
   /**
    * 身長の更新
-   * @param {state} state
-   * @param {function} height
+   * @param state
+   * @param height
    */
   height(state, { height }) {
     state.editingData.height = height;
   },
   /**
    * 体重の更新
-   * @param {state} state
-   * @param {function} weight
+   * @param state
+   * @param weight
    */
   weight(state, { weight }) {
     state.editingData.weight = weight;
   },
   /**
    * アレルギーの更新
-   * @param {state} state
-   * @param {function} allergies
+   * @param state
+   * @param allergies
    */
   allergies(state, { allergies }) {
     state.editingData.allergies = allergies;
   },
   /**
    * 病歴の更新
-   * @param {state} state
-   * @param {function} medicalHistory
+   * @param state
+   * @param medicalHistory
    */
   medicalHistory(state, { medicalHistory }) {
     state.editingData.medicalHistory = medicalHistory;
   },
   /**
    * 趣味嗜好の更新
-   * @param {state} state
-   * @param {function} preference
+   * @param state
+   * @param preference
    */
   preference(state, { preference }) {
     state.editingData.preference = preference;
   },
   /**
    * 生活歴の更新
-   * @param {state} state
-   * @param {function} lifeHistory
+   * @param state
+   * @param lifeHistory
    */
   lifeHistory(state, { lifeHistory }) {
     state.editingData.lifeHistory = lifeHistory;
   },
   /**
    * 備考の更新
-   * @param {state} state
-   * @param {function} remarks
+   * @param state
+   * @param remarks
    */
   remarks(state, { remarks }) {
     state.editingData.remarks = remarks;
@@ -99,80 +99,89 @@ export const mutations = {
 export const actions = {
   /**
    * 利用者名を設定する
-   * @param {string} name
-   * @param {function} commit
+   * @param name
+   * @param commit
    */
   setName({ commit }, { name }) {
     commit('name', { name });
   },
+
   /**
    * 生年月日を設定する
-   * @param {string} birthday
-   * @param {function} commit
+   * @param birthday
+   * @param commit
    */
   setBirthday({ commit }, { birthday }) {
     commit('birthday', { birthday });
   },
+
   /**
    * 性別を設定する
-   * @param {string} gender
-   * @param {function} commit
+   * @param gender
+   * @param commit
    */
   setGender({ commit }, { gender }) {
     commit('gender', { gender });
   },
+
   /**
    * 身長を設定する
-   * @param {string} height
-   * @param {function} commit
+   * @param height
+   * @param commit
    */
   setHeight({ commit }, { height }) {
     commit('height', { height });
   },
+
   /**
    * 体重を設定する
-   * @param {string} weight
-   * @param {function} commit
+   * @param weight
+   * @param commit
    */
   setWeight({ commit }, { weight }) {
     commit('weight', { weight });
   },
+
   /**
    * アレルギーを設定する
-   * @param {string} allergies
-   * @param {function} commit
+   * @param allergies
+   * @param commit
    */
   setAllergies({ commit }, { allergies }) {
     commit('allergies', { allergies });
   },
+
   /**
    * 病歴を設定する
-   * @param {string} medicalHistory
-   * @param {function} commit
+   * @param medicalHistory
+   * @param commit
    */
   setMedicalHistory({ commit }, { medicalHistory }) {
     commit('medicalHistory', { medicalHistory });
   },
+
   /**
    * 趣味嗜好を設定する
-   * @param {string} preference
-   * @param {function} commit
+   * @param preference
+   * @param commit
    */
   setPreference({ commit }, { preference }) {
     commit('preference', { preference });
   },
+
   /**
    * 生活歴を設定する
-   * @param {string} lifeHistory
-   * @param {function} commit
+   * @param lifeHistory
+   * @param commit
    */
+
   setLifeHistory({ commit }, { lifeHistory }) {
     commit('lifeHistory', { lifeHistory });
   },
   /**
    * 備考を設定する
-   * @param {string} remarks
-   * @param {function} commit
+   * @param remarks
+   * @param commit
    */
   setRemarks({ commit }, { remarks }) {
     commit('remarks', { remarks });
