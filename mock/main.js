42ce9c1305e9b7f40e9c2d9ff0977b38cfa767c3
seiha-tsuge <seiha.tsuge@e-tsuge.com>
2021-03-28 23:16:40 +0900

    fix: app.use(express.json())の記述位置を変更

--- mock/main.js
+++ mock/main.js
@@ -7,12 +7,13 @@ const routers = require('./routes');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Mock API is running. Hello Jbc Check.');
 });
 
 app.use('/api', routers);
-app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
