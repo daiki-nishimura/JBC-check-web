b7c589b0ecb1fd0e6661dc4f218dd8b865e76c9c
seiha-tsuge <seiha.tsuge@e-tsuge.com>
2021-05-02 14:32:48 +0900

    feat: validatorjsの初期設定を追加

---
+++ src/utils/validator/lang_ja.js
@@ -0,0 +1,9 @@
+import Validator from 'validatorjs';
+
+const ja = Validator.getMessages('ja');
+
+Validator.setMessages('ja', {
+  ...ja,
+  required: 'attributeを入力してください',
+  max: 'attributeは:max文字以内で入力してください',
+});
