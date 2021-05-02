e170b3e9ae8ec928b99f8b98acfe74f50269960b
seiha-tsuge <seiha.tsuge@e-tsuge.com>
2021-05-02 16:17:17 +0900

    feat: 入力エラーがある場合、登録ボタンを非活性にする処理を追加

--- src/components/resident/ResidentRegisterAction.vue
+++ src/components/resident/ResidentRegisterAction.vue
@@ -3,13 +3,14 @@
     <ContentEditorButtons>
       <v-btn color="cancel" @click="cancel">キャンセル</v-btn>
 
-      <v-btn class="white--text" color="primary">登録</v-btn>
+      <v-btn color="primary" :disabled="canRegister" class="white--text">登録</v-btn>
     </ContentEditorButtons>
   </v-card-actions>
 </template>
 
 <script>
 import ContentEditorButtons from '@/components/common/ContentEditorButtons.vue';
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'ResidentRegisterAction',
@@ -17,6 +18,19 @@ export default {
   components: {
     ContentEditorButtons,
   },
+
+  computed: {
+    ...mapGetters('residentEditor', ['hasErrors']),
+
+    /**
+     * 登録できるか
+     * @returns {boolean}
+     */
+    canRegister() {
+      return !this.hasErrors;
+    },
+  },
+
   methods: {
     cancel() {
       this.$router.push({
