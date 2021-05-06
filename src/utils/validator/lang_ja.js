8c64c122d013e90979ec3ba7b3316c7d6094ea4e
daiki-nishimura <daikiahe0@gmail.com>
2021-05-06 23:44:12 +0900

    feat: attributeの前に:を追加

--- src/utils/validator/lang_ja.js
+++ src/utils/validator/lang_ja.js
@@ -4,6 +4,6 @@ const ja = Validator.getMessages('ja');
 
 Validator.setMessages('ja', {
   ...ja,
-  required: 'attributeを入力してください',
+  required: ':attributeを入力してください',
   max: 'attributeは:max文字以内で入力してください',
 });
