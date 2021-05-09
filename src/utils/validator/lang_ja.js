d68f4f1c95929936511243690dfb25b92ca3de91
daiki-nishimura <daikiahe0@gmail.com>
2021-05-09 17:54:44 +0900

    refactor: :の追加

--- src/utils/validator/lang_ja.js
+++ src/utils/validator/lang_ja.js
@@ -5,5 +5,5 @@ const ja = Validator.getMessages('ja');
 Validator.setMessages('ja', {
   ...ja,
   required: ':attributeを入力してください',
-  max: 'attributeは:max文字以内で入力してください',
+  max: ':attributeは:max文字以内で入力してください',
 });
