290b8c1a0138e6331306c9f0b893999bb39dd9a8
daiki-nishimura <daikiahe1@gmail.com>
2021-05-27 18:30:37 +0900

    refactor: validator Messagesの追加修正

--- src/utils/validator/lang_ja.js
+++ src/utils/validator/lang_ja.js
@@ -4,7 +4,9 @@ const ja = Validator.getMessages('ja');
 
 Validator.setMessages('ja', {
   ...ja,
-  required: ':attributeを入力してください',
-  max: ':attributeは:max文字以内で入力してください',
-  regex: ':attributeは半角数字入力してください',
+  required: ':attributeを入力してください。',
+  max: ':attributeは:max文字以内で入力してください。',
+  integer: ':attributeは半角数字入力してください。',
+  regex: ':attributeは例のように入力してください。例）2000/01/01',
+  date: '生年月日は正しい日付形式、半角数字で入力してください。'
 });
