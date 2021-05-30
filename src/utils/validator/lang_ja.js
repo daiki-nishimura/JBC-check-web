3f4babfa0c6cb73d90a3b6c74ed28900ca96b665
daiki-nishimura <daikiahe0@gmail.com>
2021-05-30 21:25:49 +0900

    refactor: エラーメッセージ 内容を修正

--- src/utils/validator/lang_ja.js
+++ src/utils/validator/lang_ja.js
@@ -6,7 +6,7 @@ Validator.setMessages('ja', {
   ...ja,
   required: ':attributeを入力してください。',
   max: ':attributeは:max文字以内で入力してください。',
-  integer: ':attributeは半角数字入力してください。',
+  integer: ':attributeの最初の数字は0以外の半角数字で入力してください。',
   regex: ':attributeは例のように入力してください。例）2000/01/01',
-  date: '生年月日は正しい日付形式、半角数字で入力してください。'
+  date: ':attributeは正しい日付形式、半角数字で入力してください。'
 });
