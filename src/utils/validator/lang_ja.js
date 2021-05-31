c6fa6b8ef3e9995cb1fa1d2c7aaf1cef5091cee3
daiki-nishimura <daikiahe0@gmail.com>
2021-05-31 11:55:12 +0900

    refactor: regex メッセージ内容修正

--- src/utils/validator/lang_ja.js
+++ src/utils/validator/lang_ja.js
@@ -7,6 +7,6 @@ Validator.setMessages('ja', {
   required: ':attributeを入力してください。',
   max: ':attributeは:max文字以内で入力してください。',
   integer: ':attributeの最初の数字は0以外の半角数字で入力してください。',
-  regex: ':attributeは例のように入力してください。例）2000/01/01',
+  regex: ':attributeはYYYY/MM/DD形式で入力してください。例）2000/01/01',
   date: ':attributeは正しい日付形式、半角数字で入力してください。'
 });
