5daf27fd7f76c318aee38bee22ada8b39f65efeb
daiki-nishimura <daikiahe0@gmail.com>
2021-06-27 00:10:29 +0900

    refactor: corsでの許可設定

--- mock/main.js
+++ mock/main.js
@@ -1,12 +1,24 @@
 require('./migration');
 
 const express = require('express');
+const cors = require('cors');
+
 const routers = require('./routes');
 
 const app = express();
+
+
 const port = 3000;
 
+app.use(cors());
+
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:8080',
+  credentials: true,
+  optionsSuccessStatus: 200 
+}))
 
 app.get('/', (req, res) => {
   res.send('Mock API is running. Hello Jbc Check.');
